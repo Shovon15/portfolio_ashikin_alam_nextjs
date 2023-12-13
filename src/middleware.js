5a838c146721796db193d594bbd99e5b9bf2fa52
Shovon15 <shovonrnbd@gmail.com>
2023-12-13 23:26:53 +0600

    .

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,9 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request) {
 	const path = request.nextUrl.pathname;
 
-	const isPublicPath = path === "/login" || path === "/signup" || path === "/verifyemail";
+	const isPublicPath = path === "/login" || path === "/signup";
 
 	const token = request.cookies.get("token")?.value || "";
 
@@ -14,9 +14,17 @@ export function middleware(request) {
 	if (!isPublicPath && !token) {
 		return NextResponse.redirect(new URL("/login", request.nextUrl));
 	}
+
+	// Check if the path is a nested route under "/dashboard"
+	if (path.startsWith("/dashboard") && !token) {
+		return NextResponse.redirect("/login");
+	}
+
+	// Continue to the next middleware or handler
+	return null;
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-	matcher: ["/dashboard", "/login"],
+	matcher: ["/", "/dashboard", "/login"],
 };
